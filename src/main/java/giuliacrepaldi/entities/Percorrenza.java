fc9cd13056017d2f70c2a26cd72d967f794557c4
Giulia Creps <giulia.creps2@gmail.com>
2026-03-31 15:27:06 +0200

    done class Tratta and Percorrenza attribute, constructors, getters and setters

--- src/main/java/giuliacrepaldi/entities/Percorrenza.java
+++ src/main/java/giuliacrepaldi/entities/Percorrenza.java
@@ -1,4 +1,43 @@
 package giuliacrepaldi.entities;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Percorrenza {
+    private UUID percorrenzaId;
+    private LocalDateTime tempoEffettivoPercorrenza;
+
+    @ManyToOne
+    @JoinColumn(name = "tratta_id")
+    private Tratta trattaId;
+
+    @ManyToOne
+    @JoinColumn(name = "mezzo_di_trasporto")
+    private MezzoTrasporto mezzoTrasportoId;
+
+    //Costruttore
+    public Percorrenza() {
+
+    }
+
+    public Percorrenza(UUID percorrenzaId, LocalDateTime tempoEffettivoPercorrenza) {
+        this.percorrenzaId = percorrenzaId;
+        this.tempoEffettivoPercorrenza = tempoEffettivoPercorrenza;
+    }
+
+    //Getter e Setter
+    public UUID getPercorrenzaId() {
+        return percorrenzaId;
+    }
+
+    public LocalDateTime getTempoEffettivoPercorrenza() {
+        return tempoEffettivoPercorrenza;
+    }
+
+    public void setTempoEffettivoPercorrenza(LocalDateTime tempoEffettivoPercorrenza) {
+        this.tempoEffettivoPercorrenza = tempoEffettivoPercorrenza;
+    }
 }
--- src/main/java/giuliacrepaldi/entities/Tratta.java
+++ src/main/java/giuliacrepaldi/entities/Tratta.java
@@ -1,4 +1,70 @@
 package giuliacrepaldi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tratte")
+
 public class Tratta {
+    private UUID trattaId;
+    private double trattaKm;
+    private LocalDateTime tempoPrevistoTratta;
+    private String zonaPartenza;
+    private String zonaArrivo;
+
+    //Costruttore
+    public Tratta() {
+
+    }
+
+    public Tratta(UUID trattaId, double trattaKm, LocalDateTime tempoPrevistoTratta, String zonaPartenza, String zonaArrivo) {
+        this.trattaId = trattaId;
+        this.trattaKm = trattaKm;
+        this.tempoPrevistoTratta = tempoPrevistoTratta;
+        this.zonaPartenza = zonaPartenza;
+        this.zonaArrivo = zonaArrivo;
+    }
+
+
+    //Getter & Setter
+
+    public UUID getTrattaId() {
+        return trattaId;
+    }
+
+    public double getTrattaKm() {
+        return trattaKm;
+    }
+
+    public void setTrattaKm(double trattaKm) {
+        this.trattaKm = trattaKm;
+    }
+
+    public LocalDateTime getTempoPrevistoTratta() {
+        return tempoPrevistoTratta;
+    }
+
+    public void setTempoPrevistoTratta(LocalDateTime tempoPrevistoTratta) {
+        this.tempoPrevistoTratta = tempoPrevistoTratta;
+    }
+
+    public String getZonaPartenza() {
+        return zonaPartenza;
+    }
+
+    public void setZonaPartenza(String zonaPartenza) {
+        this.zonaPartenza = zonaPartenza;
+    }
+
+    public String getZonaArrivo() {
+        return zonaArrivo;
+    }
+
+    public void setZonaArrivo(String zonaArrivo) {
+        this.zonaArrivo = zonaArrivo;
+    }
 }
