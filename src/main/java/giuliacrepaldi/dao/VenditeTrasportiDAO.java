d3ad819734c32c25daf5e30421a4edeab3e40e28
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-31 14:17:00 +0200

    start vendite trasporti dao

--- src/main/java/giuliacrepaldi/dao/VenditeTrasportiDAO.java
+++ src/main/java/giuliacrepaldi/dao/VenditeTrasportiDAO.java
@@ -1,4 +1,15 @@
 package giuliacrepaldi.dao;
 
+import jakarta.persistence.EntityManager;
+
 public class VenditeTrasportiDAO {
+
+    private final EntityManager entityManager;
+    
+    public VenditeTrasportiDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    
+    
+    
 }
