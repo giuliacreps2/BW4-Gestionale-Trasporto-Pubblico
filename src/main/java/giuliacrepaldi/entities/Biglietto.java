8337b6ee79b635f4c274a0f91cd7c53d0c5d9d9a
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-31 13:44:20 +0200

    Biglietto entity done

--- src/main/java/giuliacrepaldi/entities/Biglietto.java
+++ src/main/java/giuliacrepaldi/entities/Biglietto.java
@@ -1,5 +1,6 @@
 package giuliacrepaldi.entities;
 
+import giuliacrepaldi.exceptions.biglietto.BigliettoGiaObliteratoException;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -10,10 +11,14 @@ import java.util.UUID;
 @DiscriminatorValue("biglietto")
 public class Biglietto extends VenditaTrasporto  {
     
-    // @ManyToOne
-    // @JoinColumn(name = "obliterato_da")
-    // private MezzoTrasporto obliteratoDa;
+    @ManyToOne
+    @JoinColumn(name = "obliterato_da")
+    private MezzoTrasporto obliteratoDa;
     
+    // TODO: FIX: data e ora deve contenere data e ora, non solo ora.
+    //  quale tipo java serve? (stessa cosa per fine obliterazione)
+    //  fatto questo, nel metodo setObliteratoDa, va cambiato 
+    //  anche l'assegnamento di inizio e fine obliterazione
     @Column(name = "data_e_ora_inizio_obliterazione")
     private LocalDate dataEOraInizioObliterazione;
 
@@ -21,7 +26,46 @@ public class Biglietto extends VenditaTrasporto  {
     private LocalDate dataEOraFineObliterazione;
     
     protected Biglietto() {}
+
+    public Biglietto(PuntoEmissione puntoEmissione, double prezzo) {
+        super(puntoEmissione, prezzo);
+    }
     
+    public MezzoTrasporto getObliteratoDa() {
+        return obliteratoDa;
+    }
+
+    public void setObliteratoDa(MezzoTrasporto obliteratoDa) {
+        // se il biglietto è stato già obliterato, non 
+        // può essere di nuovo obliterato
+        if(this.obliteratoDa != null) {
+            throw new BigliettoGiaObliteratoException(this);
+        }
+        
+        this.obliteratoDa = obliteratoDa;
+        
+        // imposta data inizio obliterazione
+        this.dataEOraInizioObliterazione = LocalDate.now();
+        
+        // imposta data fine obliterazione
+        this.dataEOraFineObliterazione = LocalDate.now().plusDays(1);
+    }
+
+
+    public LocalDate getDataEOraFineObliterazione() {
+        return dataEOraFineObliterazione;
+    }
     
-    
+
+    public LocalDate getDataEOraInizioObliterazione() {
+        return dataEOraInizioObliterazione;
+    }
+
+    @Override
+    public String toString() {
+        return "Biglietto{" +
+                "dataEOraFineObliterazione=" + dataEOraFineObliterazione +
+                ", dataEOraInizioObliterazione=" + dataEOraInizioObliterazione +
+                '}';
+    }
 }
