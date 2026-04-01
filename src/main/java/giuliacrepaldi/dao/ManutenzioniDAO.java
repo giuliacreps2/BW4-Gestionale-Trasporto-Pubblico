d2125892c22afea1ebf042ebca70e9e48fb4287d
Giulia Creps <giulia.creps2@gmail.com>
2026-04-01 23:13:29 +0200

    Added methods to be inserted in ManutenzioniDAO and MezziTrasportoDAO

--- src/main/java/giuliacrepaldi/dao/ManutenzioniDAO.java
+++ src/main/java/giuliacrepaldi/dao/ManutenzioniDAO.java
@@ -8,22 +8,31 @@ import jakarta.persistence.EntityTransaction;
 public class ManutenzioniDAO {
     private final EntityManager em;
 
-    public ManutenzioniDAO(EntityManager em){
+    public ManutenzioniDAO(EntityManager em) {
         this.em = em;
     }
 
-    public void save(Manutenzione newManutenzione) throws ManutenzioneSalvataggioException{
+    public void save(Manutenzione newManutenzione) throws ManutenzioneSalvataggioException {
         EntityTransaction transaction = em.getTransaction();
 
         try {
-        transaction.begin();
+            transaction.begin();
 
-        em.persist(newManutenzione);
+            em.persist(newManutenzione);
 
-        transaction.commit();
-        } catch (Exception e){
+            transaction.commit();
+        } catch (Exception e) {
             throw new ManutenzioneSalvataggioException(newManutenzione);
         }
 
     }
+
+    //Metodi
+    //1.findByMezzo
+    //2.findAll con BETWEEN — manutenzioni in un periodo
+    //3.findManutenzioniInCorso(UUID mezzoId) ---> con data di riferimento oggi
+
+    //TODO: valutare se inserirli qui in ManutenzioniDAO o se in MezzoTrasportoDAO
+    //4.boolean eInManutenzione(UUID mezzoId), dove somma manutenzioni in corso == 0
+    //5.boolean inServizio(UUID mezzoId)---> non in eInManutenzione()
 }
