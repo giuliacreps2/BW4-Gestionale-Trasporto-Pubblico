5faa8caab85505650db97cb87124f8deee49fa3e
Giulia Creps <giulia.creps2@gmail.com>
2026-04-01 14:10:00 +0200

    fixing countByMezzoAndTratta method

--- src/main/java/giuliacrepaldi/dao/PercorrenzeDAO.java
+++ src/main/java/giuliacrepaldi/dao/PercorrenzeDAO.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -43,7 +44,7 @@ public class PercorrenzeDAO {
             System.out.println("Percorrenza non trovata");
             return null;
         }
-        Query query = em.createQuery("SELECT p FROM Percorrenza p WHERE p.Id = :Id", Percorrenza.class);
+        Query query = em.createQuery("SELECT p FROM Percorrenza p WHERE p.mezzoTrasportoId.Id", Percorrenza.class);
         query.setParameter("Id", Id);
         List<Percorrenza> percorrenzeMezzi = query.getResultList();
         return percorrenzeMezzi;
@@ -55,19 +56,49 @@ public class PercorrenzeDAO {
             System.out.println("Percorrenza non trovata");
             return null;
         }
-        Query query = em.createQuery("SELECT p FROM Percorrenza p WHERE p.trattaId = :trattaId", Percorrenza.class);
+        Query query = em.createQuery("SELECT p FROM Percorrenza p WHERE p.trattaId.trattaId", Percorrenza.class);
         query.setParameter("trattaId", trattaId);
         List<Percorrenza> percorrenzeTratte = query.getResultList();
         return percorrenzeTratte;
     }
 
     //5. findByMezzoAndTratta(UUID mezzoId, UUID trattaId)
+//    public List<Percorrenza> findByMezzoAndTratta(UUID mezzoId, UUID trattaId) {
+//
+//    }
 
 
-    //6. countByMezzoAndTratta(UUID mezzoId, UUID trattaId) ---> calcolare il tempo di una percorrenza
+    //6. countByMezzoAndTratta(UUID mezzoId, UUID trattaId) --->  quante volte un mezzo ha percorso una tratta
+//public Integer countByMezzoAndTratta(UUID Id, UUID trattaId) {
+//        //dato un mezzo x
+//        //devo sapere quante volte
+//        //percorre la tratta y
+//    Query query = em.createQuery("SELECT p FROM Percorrenza p WHERE p.Id = :Id AND p.trattaId = :trattaId", Percorrenza.class);
+//    query.setParameter("Id", Id);
+//    query.setParameter("trattaId", trattaId);
+//    return risultato;
+//}
 
+    public Long countByMezzoAndTratta(UUID Id, UUID trattaId, LocalDateTime dataInizio, LocalDateTime dataFine) {
+        if (trattaId == null) {
+            System.out.println("Tratta non trovata");
+            return null;
+        }
+        if (Id == null) {
+            System.out.println("Mezzo non trovato");
+            return null;
+        }
+        Query query = em.createQuery("SELECT COUNT(p) FROM Percorrenza p WHERE p.mezzoTrasportoId.Id AND p.trattaId.trattaId AND p.dataPercorrenza BETWEEN :dataInizio AND :dataFine ");
+        query.setParameter("Id", Id);
+        query.setParameter("trattaId", trattaId);
+        query.setParameter("dataInizio", dataInizio);
+        query.setParameter("dataFine", dataFine);
+        Long volte = (Long) query.getSingleResult();
+        return volte;
+    }
 
-    //7. getTempoMedioEffettivo(UUID mezzoId, UUID trattaId) ---> calcolare il tempo medio di una 
+    //7. getTempoMedioEffettivo(UUID mezzoId, UUID trattaId) ---> calcolare il tempo di una percorrenza
 
+//   s
 
 }
