a0ad127955bcf33aaa08bb7a4660251311428407
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-02 15:19:46 +0200

    punto emissione: add custom exceptions, refactor

--- src/main/java/giuliacrepaldi/dao/PuntiEmissioneDAO.java
+++ src/main/java/giuliacrepaldi/dao/PuntiEmissioneDAO.java
@@ -1,11 +1,14 @@
 package giuliacrepaldi.dao;
 
 import giuliacrepaldi.entities.PuntoEmissione;
+import giuliacrepaldi.exceptions.miscellanous.StringaUUIDNonValidaException;
 import giuliacrepaldi.exceptions.punto_emissione.PuntoEmissioneNonTrovatoException;
 import giuliacrepaldi.exceptions.punto_emissione.PuntoEmissioneRimozioneException;
 import giuliacrepaldi.exceptions.punto_emissione.PuntoEmissioneSalvataggioException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 
 import java.util.UUID;
 
@@ -20,7 +23,7 @@ public class PuntiEmissioneDAO {
     /**
      * Salva/aggiorna un punto emissione.
      */
-    public void save(PuntoEmissione puntoEmissione) {
+    public void save(PuntoEmissione puntoEmissione) throws PuntoEmissioneSalvataggioException {
         EntityTransaction transaction = entityManager.getTransaction();
         
         try {
@@ -37,15 +40,46 @@ public class PuntiEmissioneDAO {
     /**
      * Trova un punto emissione per ID.
      */
-    public PuntoEmissione findById(UUID targetId) {
-        return entityManager.find(PuntoEmissione.class, targetId);
+    public PuntoEmissione findById(UUID targetId) throws PuntoEmissioneNonTrovatoException {
+        return findById(targetId.toString());
+    }
+
+    
+    /**
+     * Trova un punto emissione per ID.
+     */
+    public PuntoEmissione findById(String targetId) throws PuntoEmissioneNonTrovatoException, StringaUUIDNonValidaException {
+
+        TypedQuery<PuntoEmissione> query = entityManager.createQuery(
+                "SELECT p FROM PuntoEmissione p WHERE p.puntoEmissioneId = :targetId",
+                PuntoEmissione.class
+        );
+        
+        // pass params
+        try {
+            
+            query.setParameter("targetId", UUID.fromString(targetId));
+
+        } catch(IllegalArgumentException ex) {
+            throw new StringaUUIDNonValidaException(targetId);
+        }
+
+        // execute query
+        try {
+            
+            return query.getSingleResult();
+            
+        } catch (NoResultException ex) {
+            throw new PuntoEmissioneNonTrovatoException(targetId, "ID");
+        }
+
     }
 
     
     /**
      * Trova un punto emissione per ID e rimuovilo. 
      */
-    public void findByIdAndDelete(UUID id) {
+    public void findByIdAndDelete(UUID id) throws PuntoEmissioneNonTrovatoException, PuntoEmissioneRimozioneException {
         PuntoEmissione puntoEmissioneTrovato = this.findById(id);
 
         if (puntoEmissioneTrovato == null) {
@@ -66,7 +100,7 @@ public class PuntiEmissioneDAO {
     /**
      * Trova un punto di emissione per ID e aggiorna la sua citta e stato.
      */
-    public void findByIdAndUpdate(UUID id, String nuovaCitta, boolean nuovoStato) {
+    public void findByIdAndUpdate(UUID id, String nuovaCitta, boolean nuovoStato) throws PuntoEmissioneNonTrovatoException, PuntoEmissioneSalvataggioException {
         PuntoEmissione puntoEmissioneTrovato = this.findById(id);
 
         if (puntoEmissioneTrovato == null) {
@@ -76,11 +110,13 @@ public class PuntiEmissioneDAO {
         EntityTransaction transaction = entityManager.getTransaction();
         
         try {
+            
             transaction.begin();
             puntoEmissioneTrovato.setCitta(nuovaCitta);
             puntoEmissioneTrovato.setAttivo(nuovoStato);
             entityManager.persist(puntoEmissioneTrovato);
             transaction.commit();
+            
         } catch (RuntimeException e) {
             transaction.rollback();
             throw new PuntoEmissioneSalvataggioException(puntoEmissioneTrovato);
