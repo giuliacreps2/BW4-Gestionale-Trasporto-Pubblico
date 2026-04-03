d59db039e98c340dd8fbf2429f5e36c1f7266a25
Francesco Loffredo <f.loffredo00@gmail.com>
2026-04-03 10:43:05 +0200

    Metodi del Mneu Amministratore da 8 a 10

--- src/main/java/giuliacrepaldi/tests/francesco/AppTest.java
+++ src/main/java/giuliacrepaldi/tests/francesco/AppTest.java
@@ -2,18 +2,22 @@ package giuliacrepaldi.tests.francesco;
 
 
 
-import giuliacrepaldi.dao.BigliettiDAO;
-import giuliacrepaldi.dao.PuntiEmissioneDAO;
-import giuliacrepaldi.entities.Biglietto;
-import giuliacrepaldi.entities.PuntoEmissione;
+import giuliacrepaldi.dao.*;
+import giuliacrepaldi.entities.*;
 import giuliacrepaldi.enums.TipologiaPuntoEmissione;
+import giuliacrepaldi.exceptions.mezzo_trasporto.MezzoTrasportoNonTrovatoException;
 import giuliacrepaldi.exceptions.punto_emissione.PuntoEmissioneNonTrovatoException;
+import giuliacrepaldi.exceptions.tratta.TrattaNonTrovataException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
+
+import static giuliacrepaldi.tests.cristian.AppScanner.menuUtente;
 
 // qui vanno diversi tipi di test e sperimenti 
 // ad esempio, aggiungi i dati che ti interessano
@@ -42,66 +46,181 @@ public class AppTest {
         }
     }
 
-     public static void creaBiglietto(EntityManager em, Scanner scanner) {
+  // public static void creaBiglietto(EntityManager em, Scanner scanner) {
+//
+//     BigliettiDAO bigliettiDAO = new BigliettiDAO(em);
+//     PuntiEmissioneDAO puntiDAO = new PuntiEmissioneDAO(em);
+//
+//     try {
+////              // 1. Mostra punti disponibili
+////              System.out.println("=== PUNTI EMISSIONE DISPONIBILI ===");
+////              puntiDAO.findAll().forEach(p ->
+////                      System.out.println(p.getId() + " - " + p.getTipologia())
+////              );
+//
+//         // 2. Input ID
+//         System.out.print("Inserisci ID punto emissione: ");
+//         UUID idPunto = UUID.fromString(scanner.nextLine());
+//
+//         // 3. Recupero dal DB
+//         PuntoEmissione punto;
+//
+//         try {
+//             punto = puntiDAO.trovaPerId(idPunto);
+//         } catch (PuntoEmissioneNonTrovatoException e){
+//             System.out.println("Punto emissione non trovato!");
+//             return;
+//         }
+//
+//         System.out.print("Inserisci prezzo: ");
+//         double prezzo = Double.parseDouble(scanner.nextLine());
+//
+//         Biglietto biglietto = new Biglietto(punto, prezzo);
+//
+//         bigliettiDAO.salva(biglietto);
+//
+//         System.out.println("Biglietto creato!");
+//         System.out.println(biglietto);
+//
+//     } catch (RuntimeException e) {
+//         System.out.println("Errore: " + e.getMessage());
+//     }
+//}
+//    public static void menuUtente(){
+//        int scelta;
+//        do {
+//            System.out.println("MENU UTENTE");
+//            System.out.println("Scelta: ");
+//            System.out.println("1. Crea biglietto");
+//            System.out.println("2. Crea abbonamento");
+//            System.out.println("3. Ottieni zona partenza/arrivo");
+//            System.out.println("4. Verifica se un mezzo è in servizio");
+//            System.out.println("5. Verifica se un distributore è in servizio");
+//            System.out.println("0. Esci");
+//            scelta = Integer.parseInt(scanner.nextLine());
+//             switch (scelta) {
+//                 case 1:
+//                     EntityManager em = entityManagerFactory.createEntityManager();
+//                     creaBiglietto(em, scanner);
+//                     break;
+//             }
+//        }while (scelta != 0);
+//    }
 
-     BigliettiDAO bigliettiDAO = new BigliettiDAO(em);
-     PuntiEmissioneDAO puntiDAO = new PuntiEmissioneDAO(em);
 
-     try {
-//              // 1. Mostra punti disponibili
-//              System.out.println("=== PUNTI EMISSIONE DISPONIBILI ===");
-//              puntiDAO.findAll().forEach(p ->
-//                      System.out.println(p.getId() + " - " + p.getTipologia())
-//              );
 
-         // 2. Input ID
-         System.out.print("Inserisci ID punto emissione: ");
-         UUID idPunto = UUID.fromString(scanner.nextLine());
+    //INIZIO METODI MENU AMMINISTRATORE DA 8 A 13+ ESCI
+    //8
+    public static void statisticheMezzo(EntityManager em, Scanner scanner) {
 
-         // 3. Recupero dal DB
-         PuntoEmissione punto;
+        MezziTrasportoDAO mezziDAO = new MezziTrasportoDAO(em);
+        BigliettiDAO bigliettiDAO = new BigliettiDAO(em);
+        PercorrenzeDAO percorrenzeDAO = new PercorrenzeDAO(em);
+        ManutenzioniDAO manutenzioniDAO = new ManutenzioniDAO(em);
+        try {
+            System.out.print("Inserisci ID mezzo: ");
+            UUID idMezzo = UUID.fromString(scanner.nextLine());
+            MezzoTrasporto mezzo;
+            try {
+                mezzo = mezziDAO.trovaPerId(idMezzo);
+            } catch (MezzoTrasportoNonTrovatoException e) {
+                System.out.println("Mezzo non trovato!");
+                return;
+            }
+            int numeroBigliettiVidimati = bigliettiDAO.contaBigliettiVidimatiSuMezzoTrasporto(mezzo);
+            List<Percorrenza> percorrenze = percorrenzeDAO.findPercorrenzaByMezzo(idMezzo);
+            int numeroPercorrenze = percorrenze.size();
+            boolean inManutenzione = mezziDAO.eInManutenzione(mezzo);
+            boolean inServizio = mezziDAO.inServizio(mezzo);
+            System.out.println("STATISTICHE MEZZO ");
+            System.out.println("Mezzo: " + mezzo);
+            System.out.println("Numero biglietti vidimati: " + numeroBigliettiVidimati);
+            System.out.println("Numero percorrenze effettuate: " + numeroPercorrenze);
+            System.out.println("In manutenzione: " + inManutenzione);
+            System.out.println("In servizio: " + inServizio);
 
-         try {
-             punto = puntiDAO.trovaPerId(idPunto);
-         } catch (PuntoEmissioneNonTrovatoException e){
-             System.out.println("Punto emissione non trovato!");
-             return;
-         }
+        } catch (IllegalArgumentException e) {
+            System.out.println("UUID non valido!");
+        } catch (RuntimeException e) {
+            System.out.println("Errore: " + e.getMessage());
+        };
+        }
 
-         System.out.print("Inserisci prezzo: ");
-         double prezzo = Double.parseDouble(scanner.nextLine());
+        //9
+        public static void ottieniTempoPercorrenza(EntityManager em, Scanner scanner) {
 
-         Biglietto biglietto = new Biglietto(punto, prezzo);
+            PercorrenzeDAO percorrenzeDAO = new PercorrenzeDAO(em);
+            MezziTrasportoDAO mezziDAO = new MezziTrasportoDAO(em);
+            TratteDAO tratteDAO = new TratteDAO(em);
 
-         bigliettiDAO.salva(biglietto);
+            try {
+                System.out.print("Inserisci ID mezzo: ");
+                UUID idMezzo = UUID.fromString(scanner.nextLine());
 
-         System.out.println("Biglietto creato!");
-         System.out.println(biglietto);
+                System.out.print("Inserisci ID tratta: ");
+                UUID idTratta = UUID.fromString(scanner.nextLine());
 
-     } catch (RuntimeException e) {
-         System.out.println("Errore: " + e.getMessage());
-     }
-}
-    public static void menuUtente(){
-        int scelta;
-        do {
-            System.out.println("MENU UTENTE");
-            System.out.println("Scelta: ");
-            System.out.println("1. Crea biglietto");
-            System.out.println("2. Crea abbonamento");
-            System.out.println("3. Ottieni zona partenza/arrivo");
-            System.out.println("4. Verifica se un mezzo è in servizio");
-            System.out.println("5. Verifica se un distributore è in servizio");
-            System.out.println("0. Esci");
-            scelta = Integer.parseInt(scanner.nextLine());
-             switch (scelta) {
-                 case 1:
-                     EntityManager em = entityManagerFactory.createEntityManager();
-                     creaBiglietto(em, scanner);
-                     break;
-             }
-        }while (scelta != 0);
-    }
+                MezzoTrasporto mezzo;
+                Tratta tratta;
+
+                try {
+                    mezzo = mezziDAO.trovaPerId(idMezzo);
+                } catch (MezzoTrasportoNonTrovatoException e) {
+                    System.out.println("Mezzo non trovato!");
+                    return;
+                }
+
+                try {
+                    tratta = tratteDAO.trovaPerId(idTratta.toString());
+                } catch (TrattaNonTrovataException e) {
+                    System.out.println("Tratta non trovata!");
+                    return;
+                }
+
+                Duration tempoMedio = percorrenzeDAO.getTempoMedioEffettivo(idMezzo, idTratta);
+
+                long ore = tempoMedio.toHours();
+                long minuti = tempoMedio.toMinutesPart();
+
+                System.out.println("TEMPO PERCORRENZA");
+                System.out.println("Mezzo: " + mezzo);
+                System.out.println("Tratta: " + tratta);
+                System.out.println("Tempo medio effettivo: " + ore + " ore e " + minuti + " minuti");
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("UUID non valido!");
+            } catch (RuntimeException e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+
+        //10
+        public static void ottieniZonaPartenzaArrivo(EntityManager em, Scanner scanner) {
+
+            TratteDAO tratteDAO = new TratteDAO(em);
+
+            try {
+                System.out.print("Inserisci ID tratta: ");
+                UUID idTratta = UUID.fromString(scanner.nextLine());
+                Tratta tratta;
+                try {
+                    tratta = tratteDAO.trovaPerId(idTratta.toString());
+                } catch (TrattaNonTrovataException e) {
+                    System.out.println("Tratta non trovata!");
+                    return;
+                }
+                System.out.println(" ZONA PARTENZA / ARRIVO ");
+                System.out.println("Zona di partenza: " + tratta.getZonaPartenza());
+                System.out.println("Zona di arrivo: " + tratta.getZonaArrivo());
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("UUID non valido!");
+            } catch (RuntimeException e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+
+
     public static void menuAmministratore(){
         int scelta;
         do {
@@ -122,10 +241,23 @@ public class AppTest {
             System.out.println("0. Esci");
             scelta = Integer.parseInt(scanner.nextLine());
             switch (scelta){
+                case 8:
+                    EntityManager em8 = entityManagerFactory.createEntityManager();
+                    statisticheMezzo(em8, scanner);
+                    em8.close();
+                    break;
+                case 9:
+                    EntityManager em9 = entityManagerFactory.createEntityManager();
+                    ottieniTempoPercorrenza(em9, scanner);
+                    em9.close();
+                    break;
+
 
             }
         }while (scelta != 0);
     }
 
 }
+
+
 
